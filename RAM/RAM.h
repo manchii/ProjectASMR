2173f55f8c781e5b12b1ab578c7a094c9a3ad4bd
esolera <edgarasolera994@gmail.com>
2018-05-24 23:46:23 -0600

    RAM header

--- RAM/RAM.h
+++ RAM/RAM.h
@@ -1,8 +1,22 @@
 #include <systemc.h>
-SC_MODULE(Contador){
+SC_MODULE(RAM){
     sc_in <bool> en_write;
     sc_in <bool> en_read;
     sc_in <uint<32>> data_write;
     sc_in <uint<4>> direccion;
     sc_out <uint<32>> Salida;
-}
+    
+    void ram_func();
+    
+     SC_CTOR(RAM){
+        SC_METHOD(ram_func);
+            sensitive << en_write;
+            sensitive << en_read;
+            sensitive << direccion;
+            sensitive << data_write;
+    }
+    
+    
+};
+    
+    
