9b90c99be45fe3db0539f7f96e49e6760b5a525b
diegosalazarsi95 <diegosalazarsi95@gmail.com>
2018-08-02 03:40:45 -0600

    Ultima actualizaci[on de low_pass_filter en gaw

--- Proyecto2/low_pass_filter/filter_tb.cpp
+++ Proyecto2/low_pass_filter/filter_tb.cpp
@@ -10,8 +10,8 @@ int sc_main (int argc, char* argv[]){
 
 	sca_eln::sca_node_ref gnd;
 
-	sca_eln::sca_vsource src("src",0.0, 0.0, 3.3, 1e9);
-	src.set_timestep(1e-6, sc_core::SC_US);
+	sca_eln::sca_vsource src("src",0.0, 0.0, 3.3, 1e8);
+	src.set_timestep(1e-4, sc_core::SC_US);
 
 	src.p(in);
 	src.n(gnd);
@@ -21,16 +21,19 @@ int sc_main (int argc, char* argv[]){
 	DUT_FILTER.out(out);
 
 	//Comienza la simulacion
-	sca_util::sca_trace_file *eln= sca_util::sca_create_vcd_trace_file("filter.vcd");
+	sca_util::sca_trace_file *eln= sca_util::sca_create_tabular_trace_file("filter.dat");
 	sca_trace(eln, in, "Input_Analog");
 	sca_trace(eln, out, "Output_Analog");
 
-	sc_start(150, sc_core::SC_MS);
+	//eln->disable();
+	sc_start(1e-2, sc_core::SC_MS);
+	//eln->enable();
 
-	sca_util::sca_close_vcd_trace_file(eln);
-
+	//sca_ac_start(1e3,1e9,1000,sca_ac_analysis::SCA_LOG);
+	//ac_analysis->set_mode(sca_sampling(1.0,SC_US));
+	
+	//sca_util::sca_close_tabular_trace_file(ac_analysis);
+	sca_util::sca_close_tabular_trace_file(eln);
 
 	return 0;
-
-
 }
